9bb51773376e1195b397b05011c033866a1e0541
zahidfahim <zahidulislamfahim88@gmail.com>
2024-05-22 18:56:16 +0600

    chart added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {
+        firstName:"Zahid",
+    }
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user = {loggedIn?.firstName || "Guest"}
+                subtext = "Explore and manage your account and transactions efficiently."
+
+                />
+                <TotalBalanceBox 
+                 accounts = {[]}
+                 totalBanks={1}
+                 totalCurrentBalance={2500.75}
+                
+                />
+
+            </header>
+        </div>
+    </section>
   )
 }
 
