4269ca90cb1787d615445d43a6cbae3ae7468d0b
zahidfahim <zahidulislamfahim88@gmail.com>
2024-05-23 13:14:54 +0600

    left sidebar added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,15 @@
-
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Zahid', lastName: 'Fahim' };
   return (
-   <main>
-    Sidebar
-    {children}
-   </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
+    </main>
   );
 }
